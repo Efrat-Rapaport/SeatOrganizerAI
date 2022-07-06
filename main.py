2e77de00970e0e30b141339c372a417cfe6474ad
Nechama <neproject2@gmail.com>
2022-07-06 17:03:16 +0300

    model and server

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import server
 
 
-# # Press the green button in the gutter to run the script.
+# # Press the green button in the gutter to run the scri
 if __name__ == '__main__':
     my_server = server.myServer()
 # # See PyCharm help at https://www.jetbrains.com/help/pycharm/
